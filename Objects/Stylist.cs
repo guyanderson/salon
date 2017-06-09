cd267f4fedc5015a999a70e00499ce0ed895641e
guy <luckythelich@hotmail.com>
2017-06-09 14:28:13 -0700

    blah

---
+++ Objects/Stylist.cs
@@ -0,0 +1,117 @@
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System;
+
+namespace Stylist_objects
+{
+  public class Stylist
+  {
+    private int _id;
+    private string _name;
+//===========================================
+    public Stylist(string Name, int Id = 0)
+    {
+      _id = Id;
+      _name = Name;
+    }
+//===========================================
+    public int GetId()
+    {
+      return _id;
+    }
+//===========================================
+    public string GetName()
+    {
+      return _name;
+    }
+//===========================================
+    public void SetName(string newName)
+    {
+      _name =newName;
+    }
+//===========================================
+    public static List<Stylist> GetAll()
+    {
+      List<Stylist> allStylist = new List<Stylist>{};
+
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM stylists;", conn);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        int stylistId = rdr.GetInt32(0);
+        string stylistName = rdr.GetString(1);
+        Stylist newStylist = new Stylist(stylistName, stylistId);
+        allStylist.Add(newStylist);
+      }
+
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+
+  return allStylist;
+}
+//============================================
+    public static void DeleteAll()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("DELETE FROM stylists;", conn);
+      cmd.ExecuteNonQuery();
+      conn.Close();
+    }
+//============================================
+    public override bool Equals(System.Object otherStylist)
+    {
+      if (!(otherStylist is Stylist))
+      {
+        return false;
+      }
+      else
+      {
+        Stylist newStylist = (Stylist) otherStylist;
+        bool idEquality = (this.GetId() == newStylist.GetId());
+        bool nameEquality = (this.GetName() == newStylist.GetName());
+        return (nameEquality);
+      }
+    }
+//============================================
+    public void Save()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("INSERT INTO stylists (name) OUTPUT INSERTED.id VALUES (@StylistName)", conn);
+
+      SqlParameter nameParameter = new SqlParameter();
+      nameParameter.ParameterName = "@StylistName";
+      nameParameter.Value = this.GetName();
+      cmd.Parameters.Add(nameParameter);
+
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        this._id = rdr.GetInt32(0);
+      }
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+    }
+//============================================
+  }
+}
---
+++ Tests/StylistTest.cs
@@ -0,0 +1,32 @@
+using Xunit;
+using System.Collections.Generic;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using Stylist_objects;
+
+namespace StylistTest_Test
+{
+ [Collection("hair_salon")]
+  public class StylistTest : IDisposable
+  {
+    public StylistTest()
+    {
+      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=hair_salon_test;Integrated Security=SSPI;";
+    }
+//===========================================================
+    [Fact]
+    public void GetAll_DatabaseEmptyAtFirst_True()
+    {
+      int result = Stylist.GetAll().Count;
+
+      Assert.Equal(0, result);
+    }
+//===========================================================
+    public void Dispose()
+    {
+      Stylist.DeleteAll();
+    }
+//==========================================================
+  }
+}
